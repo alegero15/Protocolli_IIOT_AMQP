2e1596f00b689db122c24c52aa8ddfd799876db9
New Holland <133649873+alegero15@users.noreply.github.com>
2024-05-02 17:20:32 +0200

    02_05_2024 AMQP

---
+++ Receive/Receive.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+internal class Receive
+{
+    private static void Main(string[] args)
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.HostName = "goose.rmq2.cloudamqp.com";
+        factory.UserName = "myhgmgcy";
+        factory.Password = "t2A20sj74kIddDIM9x52VkElgp29ISye";
+        factory.VirtualHost = "myhgmgcy";
+        factory.Port = 5672;
+
+        using var connection = factory.CreateConnection();
+
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "hello2",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        Console.WriteLine(" [*] Waiting for messages.");
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] Received {message}");
+        };
+        channel.BasicConsume(queue: "hello2",
+                             autoAck: true,
+                             consumer: consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+//var factory = new ConnectionFactory { HostName = "amqps://myhgmgcy:t2A20sj74kIddDIM9x52VkElgp29ISye@goose.rmq2.cloudamqp.com/myhgmgcy" };
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "goose.rmq2.cloudamqp.com";
+factory.UserName = "myhgmgcy";
+factory.Password = "t2A20sj74kIddDIM9x52VkElgp29ISye";
+factory.VirtualHost = "myhgmgcy";
+factory.Port = 5672;
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello2",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+const string message = "Hello World2!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty,
+                     routingKey: "hello2",
+                     basicProperties: null,
+                     body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
